d84a7e4c9f2d9cc78372bdb2e73ac8d36e6cbcb3
Charles Davis <charlesdavis@cfmmacbook1-2.local>
2015-03-06 17:31:53 -0500

    added contact info

--- wp-content/themes/ariva/footer.php
+++ wp-content/themes/ariva/footer.php
@@ -8,13 +8,23 @@
     */
     global $ts_ariva;
 ?>
-        <footer>
+        <footer id="page-footer" class="page-footer">
             <div class="container">
-                    <?php get_template_part('content-parts/footer', 'socials'); ?>
-                <div id="flags_language_selector"><?php language_selector_flags(); ?></div >
-                <div class="copyright">
-                    <?php echo apply_filters('the_content',  $ts_ariva['footer_copyright_text']); ?>
-                </div>
+                <?php
+                    $args = array( 'page_id' => 14 );
+                    // The Query
+                    $the_query = new WP_Query( $args );
+                    // The Loop
+                    if ( $the_query->have_posts() ) {
+                        while ( $the_query->have_posts() ) {
+                            $the_query->the_post();
+                            echo '<div class="whole">';
+                                the_content();
+                            echo '</div>';
+                        }
+                    } 
+                    wp_reset_postdata();
+                ?>           
             </div>
         </footer>
 
