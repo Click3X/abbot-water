56fc82da5347301b0145a1dbf8fcf38d7f9cb4f0
Charles Davis <charles@click3x.com>
2016-03-04 15:48:41 -0500

    Finished updating passwords for users, db, and updating wp, plugins, as well las deleting all unused files

--- wp-content/themes/ariva copy/content-parts/footer-socials.php
+++
@@ -1,8 +0,0 @@
-<?php global $ts_ariva;?>
-<ul class="social-footer">
-<?php foreach ($ts_ariva['ts-social-list'] as $value): ?>
-	<?php if ($value['url'] !=''): ?>
-  		<li class="bounceIn animated"><a target="_blank" href="<?php echo esc_url($value['url']); ?>"><i class="fa fa-<?php echo esc_attr($value['title']); ?> "></i></a></li>
-	<?php endif ?>
-<?php endforeach ?>
-</ul>
